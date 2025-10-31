671dbfb55f9b91af5a312906a45eb31364ff46c0
jihojaty-tech <jihojaty@gmail.com>
2025-10-31 17:32:51 +0900

    html_25_10_28 add

--- day12/09_Exam01.js
+++ day12/09_Exam01.js
@@ -1,24 +1,40 @@
+const num1 = [];
+const num2 = [];
+const result = [];
+
 // 문제 랜덤 생성 함수
 function quizRandom(){
-    num1 = Math.floor(Math.random()*9)+1;
-    num2 = Math.floor(Math.random()*9)+1;
-    return num1,num2;
+    for(let i = 0; i < 10; i++){
+        num1[i] = Math.floor(Math.random()*9)+1;
+        num2[i] = Math.floor(Math.random()*9)+1;
+        
+    }
+    
+    return [num1, num2];
 }
-
+// 버튼 클릭하면 물제 출제 함수
+document.getElementById('quiz').addEventListener('click',()=>{
+    const [nums1, nums2] = quizRandom();
+    for(let i = 0; i < 10; i++){
+        document.getElementById(i+1).innerText = `${nums1[i]} x ${nums2[i]} = `;
+    }
+})
 // 정답 확인 함수
 function quizCheck(){
-    let quiz = quizRandom();
+    
+  
+
 }
-console.log(quiz);
 
 
 
-function PersonClassObject(first, last, age, addr){
-            this.firstName = first;
-            this.lastName = last;
-            this.age = age;
-            this.address = addr;
-            this.getPersonInfo = function(){
-                return `${this.firstName}${this.lastName} (${this.age}) / ${this.address}`;
-            }
-        }
+
+// function PersonClassObject(first, last, age, addr){
+//             this.firstName = first;
+//             this.lastName = last;
+//             this.age = age;
+//             this.address = addr;
+//             this.getPersonInfo = function(){
+//                 return `${this.firstName}${this.lastName} (${this.age}) / ${this.address}`;
+//             }
+//         }
